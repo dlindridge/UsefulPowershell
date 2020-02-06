baf0b0d343040d892ac1f218a00f8c8d7ffb8740
Derek Lindridge <56898125+dlindridge@users.noreply.github.com>
2020-02-06 10:15:41 -0800

    Add files via upload

---
+++ FindEventID.ps1
@@ -0,0 +1,56 @@
+<#
+	.SYNOPSIS
+	Looks through the event log to find a specific Event ID number and outputs results to file choice (CSV or TXT).
+
+	.PARAMETER Path
+	Directory to place output file(s) to.
+
+	.PARAMETER EventID
+	Event ID number to search for.
+	
+	.PARAMETER CSV
+	Switch - Designates output should go to a CSV file. This is good for determining when and who triggered an event.
+	
+	.PARAMETER TXT
+	Switch - Designates output should go to a TXT file. This is good for determining WHY an event was triggered.
+
+	.DESCRIPTION
+	Usage: .\FindEventID.ps1 -Path D:\Some\Directory -EventID 1074 (-CSV) (-TXT)
+#>
+#################################################
+<#
+Author: Derek Lindridge
+https://www.linkedin.com/in/dereklindridge/
+Created: Feb 03, 2020
+Modified: Feb 03, 2020
+#>
+#################################################
+
+Param (
+    [Parameter(Mandatory=$True)]
+    $Path,
+    [Parameter(Mandatory=$True)]
+    $EventID,
+	[switch]$CSV,
+	[switch]$TXT
+)
+
+# Get local computer name
+$CompName = $ENV:computername
+
+# Format output files
+$Date = (Get-Date).ToString("yyyyMMMdd-HHmmss")
+$CSVOut = "$CompName-$Date-EID$EventID.csv"
+$TXTOut = "$CompName-$Date-EID$EventID.txt"
+$CSVOutFile = Join-Path -Path $Path -ChildPath $CSVOut
+$TXTOutFile = Join-Path -Path $Path -ChildPath $TXTOut
+
+# Generate CSV output file
+If ($CSV -eq $True) {
+	Get-EventLog -LogName System | Where-Object {$_.EventID -eq $EventID} | Export-CSV $CSVOutFile -NoTypeInformation
+	}
+
+# Generate Format List output as TXT file
+If ($TXT -eq $True) {
+	Get-EventLog -LogName System | Where-Object {$_.EventID -eq $EventID} | fl | Out-File $TXTOutFile
+	}
