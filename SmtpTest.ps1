4c233e86d073a802acb1cf6afdec5429afbb9f59
Derek Lindridge <56898125+dlindridge@users.noreply.github.com>
2022-10-06 11:23:11 -0700

    Add files via upload

--- SmtpTest.ps1
+++ SmtpTest.ps1
@@ -8,8 +8,8 @@
         .PARAMETER Recipient
         The destination email address.
 
-		.DESCRIPTION
-		Usage: .\SmtpTest.ps1 (-Server smtprelay.somedomain.net)
+        .DESCRIPTION
+        Usage: .\SmtpTest.ps1 (-Server smtprelay.somedomain.net)
     #>
 
 #################################################
@@ -36,8 +36,8 @@ $LocalHost = Get-Content ENV:ComputerName
 $From = "SMTP Test <noreply@mydomain.tld>" # Enter the FROM address for the e-mail alert - Must be inside quotes.
 
 $body = "<p style='font-family:calibri'>This is a test of your SMTP relay settings and authorizations on $Server.</p>
-		<p style='font-family:calibri'>If you are reading this, it worked.</p>
-		<p style='font-family:calibri'>This test was run from $LocalHost.</p>"
+         <p style='font-family:calibri'>If you are reading this, it worked.</p>
+         <p style='font-family:calibri'>This test was run from $LocalHost.</p>"
 
 Send-MailMessage -To $Recipient -From $From -Subject "Info: SMTP Relay Test" -Body "$body" -SmtpServer $Server -BodyAsHtml
 
